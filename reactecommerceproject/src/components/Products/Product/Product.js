2f163ba286dbff7ee4d6ce5d1e8ae3e5d5aa3859
Parker Manning <parkerlmanning@hotmail.com>
2021-01-26 01:51:59 -0600

    correct response from commercejs showing on screen/ correct mapping of products array

--- reactecommerceproject/src/components/Products/Product/Product.js
+++ reactecommerceproject/src/components/Products/Product/Product.js
@@ -3,26 +3,26 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ( product ) => {
     // import styles from material ui at styles.js
     const classes = useStyles();
 
     console.log(product,"inside Product.js")
-        return <div>test</div>
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.product.media.source} title={product.product.name} />
             <CardContent>
                 <div className={classes.CardContent}>
                     <Typography variant="h5" gutterBottom>
-                        {product.name}
+                        {product.product.seo.title}
                     </Typography>
                     <Typography variant="h5">
-                        {product.price}
+                        {product.product.price.formatted_with_code}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary">
-                        {product.description}
+                <Typography variant="h6" color="textSecondary">
+                        {product.product.seo.description}
                 </Typography>
             </CardContent>
             <CardActions disableSpacing className={classes.CardActions}>
