94a5a2d142ef137972fab975546463fc1edaf1bf
Parker Manning <parkerlmanning@hotmail.com>
2021-01-25 22:49:20 -0600

    added in pictures - hardcoded - , working on navBar

--- reactecommerceproject/src/components/Navbar/Navbar.js
+++ reactecommerceproject/src/components/Navbar/Navbar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import shopImg from '../../assets/PinClipart.com_ramp-clipart_3220521.png'
 
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <AppBar position="fixed" className={classes.appbar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={shopImg} alt="commerce site" height="25px" className={classes.image} />
+                        Commerce Practice Site
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
